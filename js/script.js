d523726439e85be6615d17db4c5c2fcbc9c700e2
Aleksey <axcerz@gmail.com>
2021-07-25 16:59:09 +1000

    method-POST format-JSON

--- js/script.js
+++ js/script.js
@@ -192,7 +192,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
 		'img/tabs/elite.jpg',
 		'elite',
 		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		`В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное 
+		исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода 
+		в ресторан!`,
 		'16',
 		'.menu .container').bildDivMenu();
 
@@ -200,7 +202,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
 		'img/tabs/vegy.jpg',
 		'vegy',
 		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		`Меню "Фитнес" - это новый подход к приготовлению блюд: больше 
+		свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт 
+		с оптимальной ценой и высоким качеством!`,
 		'6',
 		'.menu .container',
 		'menu__item').bildDivMenu();
@@ -209,8 +213,66 @@ document.addEventListener('DOMContentLoaded', ()=> {
 		'img/tabs/post.jpg',
 		'post',
 		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		`Меню “Постное” - это тщательный подбор ингредиентов: 
+		полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, 
+		правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
 		'12',
 		'.menu .container',
 		'menu__item').bildDivMenu();
+
+	// Forms
+
+	const forms							= document.querySelectorAll('form'),
+			message						= {
+				loading: 'Загрузка...',
+				success: 'Спасибо! Наша команда скоро свяжется с вами!',
+				failure: 'Охх... Что-то пошло не так...'
+			};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (e)=> {
+			e.preventDefault();
+	
+			const statusMessage			= document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent	= message.loading;
+			form.append(statusMessage);
+
+			const request					= new XMLHttpRequest();
+			request.open('POST', 'server.php');
+
+			request.setRequestHeader('Content-type', 'application/json');
+			
+			const formData					= new FormData(form);
+
+			const object					= {};
+			formData.forEach((value, key)=> {
+				object[key] = value;
+			});
+
+			const jsonObject = JSON.stringify(object);
+
+			request.send(formData);
+
+			request.addEventListener('load', ()=> {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(()=> {
+						statusMessage.remove();
+					}, 3000);
+				} else {
+					statusMessage.textContent = message.failure;
+					setTimeout(()=> {
+						statusMessage.remove();
+					}, 5000);
+				}
+			});
+		});
+	}
 });
